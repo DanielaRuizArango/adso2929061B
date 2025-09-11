72494b45b47cbf85a9fa389cd444aa7af572f2b7
daniela-ruiz <daruiz1218@gmail.com>
2025-09-10 23:35:08 -0500

    Add memory game challenge HTML and JavaScript: Created a new memory game page with game logic, styles, and interactive elements. Updated navigation SVG in template for consistency.

---
+++ 14-php/14-arrays-multi.php
@@ -0,0 +1,23 @@
+<?php
+$tittle = '14- Arrays Multidimensional.php';
+$description = 'Arrays that contain other nested arrays';
+
+include 'template/header.php';
+
+echo "<section>";
+
+$bicycles = array(
+    'Specialized' => array('Enduro', 'Stumpjumper', 'Camber'),
+    'Intense' => array('Dirt', 'Cross', 'Rally'),
+    'Santa Cruz' => array('Hightroad', 'Nomad', 'Blade'),
+);
+
+//var_dump($bicycles);
+
+foreach ($bicycles as $key => $value ) {
+    echo "<h3>$key</h3>";
+    echo "<ul>";
+    foreach ($value as $refer) {
+        echo . "<li>$refer</li>";
+    }
+}
