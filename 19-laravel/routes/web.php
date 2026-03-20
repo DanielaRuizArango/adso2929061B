1684508313586f6dedbd84c4219f6219d52d1147
DanielaRuizArango <daniela_ruizar@soy.sena.edu.co>
2026-03-19 19:12:28 -0500

    add views y controllers

--- 19-laravel/routes/web.php
+++ 19-laravel/routes/web.php
@@ -108,5 +108,28 @@ Route::get('showpet/{id}', function() {
     return view('showpet')->with('pet', $pet);
 });
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// Resources
+Route::middleware('auth')->group( function () {
+    Route::resources([
+        'users'    => UserController::class,
+        'pets'     => PetController::class,
+        'adoption' => AdoptionController::class
+    ]);
+});
+
 
 require __DIR__.'/auth.php';
