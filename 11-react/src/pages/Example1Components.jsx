ac75ca0635e45ddd8cbba6cd63c1c5f2515c1f3b
DanielaRuizArango <daniela_ruizar@soy.sena.edu.co>
2026-05-27 20:59:42 -0500

    feat: add Bulbasaur, Charmander, and Squirtle components; update Example1 and Example2 headings and structure

--- 11-react/src/pages/Example1Components.jsx
+++ 11-react/src/pages/Example1Components.jsx
@@ -1,10 +1,45 @@
 import BtnBack from '../components/BtnBack';
 
+function Bulbasaur () {
+    return (
+        <div style ={{ border: '4px solid lightblue', padding: '1.4rem', borderRadius: '0.4rem', background:'green', width:'360px'}}>
+            <h2>Bulbasur</h2>
+            <p>Type: Grass</p>
+            <p>Ability: Razor Leaf</p>
+        </div>
+    )
+}
+
+function Charmander () {
+    return (
+        <div style ={{ border: '4px solid lightblue', padding: '1.4rem', borderRadius: '0.4rem', background:'orange', width:'360px'}}>
+            <h2>Charmander</h2>
+            <p>Type: Fire</p>
+            <p>Ability: Blaze</p>
+        </div>
+    )
+}
+
+
+function Squirtle () {
+    return (
+        <div style ={{ border: '4px solid lightblue', padding: '1.4rem', borderRadius: '0.4rem', background:'blue', width:'360px'}}>
+            <h2>Squirtle</h2>
+            <p>Type: Water</p>
+            <p>Ability: Torrent</p>
+        </div>
+    )
+}
+
 function Example1Components() {
     return(
         <div className="container">
             <BtnBack />
-        <h2>Example1Components</h2>
+        <h2>Example1: Components</h2>
+        <p>Create indepent a reusable UI pieces.</p>
+        <div style={{display:'flex', flexWrap:'wrap', jus}}>
+
+        </div>
         </div>
     );
 }
--- 11-react/src/pages/Example2JSX.jsx
+++ 11-react/src/pages/Example2JSX.jsx
@@ -1,10 +1,14 @@
 import BtnBack from '../components/BtnBack';
 
+const url ='';
+
 function Example2JSX() {
     return(
         <div className="container">
             <BtnBack />
-        <h2>Example2JSX</h2>
+        <h2>Example2: JSX</h2>
+        <p></p>
+        <img src={url} alt="" />
         </div>
     );
 }
